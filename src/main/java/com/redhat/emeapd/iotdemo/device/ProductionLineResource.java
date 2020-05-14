7579414010c4f5cd93e62d026aea20f977b2f223
abattagl <abattagl@localhost>
2020-05-14 17:08:57 +0200

    Added Basic Quarkus-maven archetype Added basic prod and cooling properties

---
+++ src/main/java/com/redhat/emeapd/iotdemo/device/ProductionLineResource.java
@@ -0,0 +1,16 @@
+//package com.redhat.emeapd.iotdemo.device;
+//
+//import javax.ws.rs.POST;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
+//import javax.ws.rs.core.MediaType;
+//
+//@Path("/productline")
+//public class ProductionLineResource {
+//
+//    @POST
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String hello() {
+//        return "hello";
+//    }
+//}
